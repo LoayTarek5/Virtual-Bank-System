29a0383a3e19dce867db0a33066528e359d80d19
ahmedsmoselhi <ahmedsmoselhi@gmail.com>
2026-07-22 18:15:27 +0300

    chore(user): initialize spring boot  with java 21

--- user-service/src/main/java/com/vbank/user/UserServiceApplication.java
+++ user-service/src/main/java/com/vbank/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.vbank.user_service;
+package com.vbank.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- user-service/src/test/java/com/vbank/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/vbank/user/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.vbank.user_service;
+package com.vbank.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
