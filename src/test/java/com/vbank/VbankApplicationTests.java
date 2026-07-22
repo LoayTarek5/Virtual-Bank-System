427fdfa8982a820e36de837f90839e09f7937c33
LoayTarek5 <loaytareq44@gmail.com>
2026-07-22 16:08:08 +0300

    Set up multi-module Maven project with Java 21 and Spring Boot 4.1

---
+++ src/test/java/com/vbank/VbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
