193dd78e0d832027f7d5906fdaf264c55309b58d
ahmedsmoselhi <ahmedsmoselhi@gmail.com>
2026-07-27 18:45:28 +0300

    feat: Create explicit `KafkaConfig` and `ObjectMapper` to resolve auto-configuration limitations

--- logging-service/src/main/java/com/vbank/logging/LoggingServiceApplication.java
+++ logging-service/src/main/java/com/vbank/logging/LoggingServiceApplication.java
@@ -1,12 +1,22 @@
 package com.vbank.logging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LoggingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoggingServiceApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
     }
 }
